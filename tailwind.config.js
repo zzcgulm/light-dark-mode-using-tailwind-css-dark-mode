87e66a728b16cfec7927afc91cc9cb21b62e5ae2
leigh.morris <leigh.morris@jisc.ac.uk>
2024-06-13 15:34:58 +0100

    Revert "Revert "Adds demo of Tailwind CSS Dark mode; updates config to suit.""
    This reverts commit 74afdc5129887d90d69ef9b4fbffd948817b63cf.

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ module.exports = {
     preflight: false,
   },
   theme: {
+    colors: {
+      bigStone: "#123",
+      beige: "#f5f5dc",
+      black: "#000", // Hmmm.
+      brownDerby: "#492615",
+      concrete: "#f2f2f2",
+      chathamsBlue: "#147",
+      doveGrey: "#666",
+      emperor: "#555",
+      mineShaft: "#333",
+      silver: "#ccc",
+      tundora: "#404040",
+      white: "#fff", // Hmmm.
+    },
     extend: {},
   },
   plugins: [],

cd7d470f6dbfed107394c748acff9abf1ce35614
leigh.morris <leigh.morris@jisc.ac.uk>
2024-06-13 13:42:44 +0100

    Installs and configures Tailwind CSS.

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  corePlugins: {
+    preflight: false,
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
